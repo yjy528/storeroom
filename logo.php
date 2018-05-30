9ecdeff86f46ff0701790e6aaa88ae7fa203e71b
yjy528 <1059178341@qq.com>
2018-05-31 00:32:00 +0800

    workdtwo

--- logo.php
+++ logo.php
@@ -2,13 +2,43 @@
    header("content-type:text/html;charset=utf-8");
    $GetUsername=$_POST['user'];
    $GetPaassWWord=$_POST['password'];
-   $MyUser='yejinyan';
-   $MyPassWord='123456';
-   
+   // $MyUser='yejinyan';
+   // $MyPassWord='123456';
+   $myInfo=array(
+       array(
+            "name"=>"zhang",
+            "pswd"=>"123333",
+       ),array(
+            "name"=>"lizi",
+            "pswd"=>"789999",
+       ),array(
+            "name"=>"wangwu",
+            "pswd"=>"456666",
+       ),array(
+            "name"=>"zhaosi",
+            "pswd"=>"258888",
+       ),
+   );
+    foreach($myInfo as $key=>$val){ 
+      
+      // list($val["name"])=$val["pswd"];
+      // echo $val["name"];
+      // echo $val["pswd"];
+      // echo "<br/>";
 
-    if($GetUsername==$MyUser&&$GetPaassWWord==$MyPassWord){
-     	echo '登录成功';
-    }else{
-        echo '用户名或密码错误';
-    }
+      if($GetUsername==$val["name"])
+      {
+        if($GetPaassWWord==$val["pswd"]){
+       	   $state="0";
+           break;
+        }else{
+           $state="1";
+           break;
+        };
+      }else{ 
+           $state="2";          
+      };
+    };
+    echo $state;
+  // };
 ?>
